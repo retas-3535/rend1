c7e8ae2a69f098ce05c51bb2abf96eb1e71235b8
retasaltyapi <100473920+retasaltyapi@users.noreply.github.com>
2025-07-26 03:11:37 +0300

    34

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
+import Toaster from './components/ui/toaster';
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 createRoot(document.getElementById("root")!).render(
