d9a5836ab42e17f775bfd4fbb7303b25839b2ccc
retasaltyapi <100473920+retasaltyapi@users.noreply.github.com>
2025-07-26 03:14:04 +0300

    SD

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,7 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import Toaster from './components/ui/toaster';
-import { TooltipProvider } from "@/components/ui/tooltip";
-
+import Tooltip from './components/ui/tooltip';
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
